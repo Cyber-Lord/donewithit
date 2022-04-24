58decd23fd61d74ee26f4ba35a30cb3fc0485b83
Cyber-Lord <Umaralfuruq02@gmail.com>
2022-04-24 16:15:11 +0100

    Added Formik & Yup

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
+import ErrorMessages from "../components/ErrorMessages";
+import Screen from "../components/Screen";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function LoginScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ errors, handleChange, handleSubmit, setFieldTouched, touched }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onBlur={() => setFieldTouched("email")}
+              onChangeText={handleChange("email")}
+              placeholder="Email"
+            />
+            <ErrorMessages error={errors.email} visible={touched.email} />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onBlur={() => setFieldTouched("password")}
+              onChangeText={handleChange("password")}
+              placeholder="Password"
+              secureTextEntry
+            />
+            <ErrorMessages error={errors.password} visible={touched.password} />
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+}
+const styles = StyleSheet.create({
+  logo: {
+    height: 80,
+    width: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 30,
+  },
+  container: {
+    padding: 10,
+  },
+});
+export default LoginScreen;
